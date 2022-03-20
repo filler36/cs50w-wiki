1774a25004bd48611ebff0b64cf4e1d155351d21
Filip Chkhaidze <filip_chkhaidze@epam.com>
2022-03-20 23:52:15 +0100

    mod

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,26 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def wiki(request, article_title):
+    if article_title in util.list_entries():
+        article_content = util.get_entry(article_title)
+        print(article_content)
+        return render(request, "encyclopedia/article.html", {
+            "article_title": article_title,
+            "article_content": article_content
+        })
+    else:
+        return render(request, "encyclopedia/not_found.html", {
+            "article_title": article_title
+        })
+
+
+def search(request, article_title):
+    if article_title in list(map(str.lower, util.list_entries())):
+        article_content = util.get_entry(article_title)
+        return render(request, "encyclopedia/article.html", {
+            "article_title": article_title,
+            "article_content": article_content
+        })
+
